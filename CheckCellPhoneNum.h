9f60f28699d75872e6b683be6ad87f94a63321e6
douya <douya@douyadeMacBook-Air.local>
2015-07-29 11:35:02 +0800

    检查手机号码的正确性

---
+++ CheckCellPhoneNum.h
@@ -0,0 +1,14 @@
+//
+//  CheckCellPhoneNum.h
+//  QQ
+//
+//  Created by user on 14-4-4.
+//  Copyright (c) 2014年 ZhangZeNan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CheckCellPhoneNum : NSObject
+
++ (BOOL)isMobileNumber:(NSString *)mobileNum;
+@end
